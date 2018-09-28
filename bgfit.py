a5c51cdbb241dd3b108dd11a3c52175afaf5e85b
SCBera <subhas.9874@gmail.com>
2018-09-28 20:45:07 +0530

    trying lsq

--- bgfit.py
+++ bgfit.py
@@ -36,23 +36,20 @@ def get_filelist(dir_, filetype='*.tif'):
     return files[1:]
 
 
-def func(x, a, b, c):
+def func_poly(x, a, b, c):
     return a*x*x + b*x + c
 
 def get_row(frame):
 
-    X = range(frame1.shape[1])
-    n = 0
+    X = range(frame.shape[1])
     fit_data = []
 
     for row in frame:
         Y = row
-        params = curve_fit(func, X, Y)
+        params = curve_fit(func_poly, X, Y)
         [a,b,c] = params[0]
-        fit_data.append(func(X, a, b, c))
-        #fit_data = np.array([list(X), func(X, a, b, c)])
-        #
-    return np.array(fit_data)
+        fit_data.append(func_poly(X, a, b, c))
+    return np.array(fit_data, dtype = frame.dtype)
 
 
 #        n += 1
@@ -62,8 +59,6 @@ def get_row(frame):
 def save_csv(dir_out, result):
     """This will save resultant values in the output folder as a '.csv' format"""
     try:
-        # np.savetxt(f"{dir_out}_results_from_{len(list_of_files)}-files.csv",
-        #            result.T, delimiter=",", header='Time(h), Avrg_int, SD, SE, Sum_int, Max_int')
         np.savetxt(f"{dir_out}bg_fit_data.csv", result.T, delimiter=",", header='X, fit_data')
     except:
         print("Existing csv file is not accessible!")
@@ -89,44 +84,47 @@ dir_out = make_dir_out(dir_)
 
 #stack = io.imread('test_stack1.tif')
 stack = io.imread(list_of_files[0])
+print(stack.shape)
+if stack.shape[0] > 200:
+    frame = stack[:, :, 1]
+else:
+    frame = stack[1]
 
-
-frame1 = stack[0]
-#print(frame1)
-#get_row(frame1)
-frame_row = frame1[100]
-X = range(frame1.shape[1])
+print(frame.shape)
+#get_row(frame)
+frame_row = frame[100]
+X = range(frame.shape[1])
 Y = frame_row
 
-params = curve_fit(func, X, Y)
+params = curve_fit(func_poly, X, Y)
 [a,b,c] = params[0]
 
 
-#io.imshow(frame1)
+#io.imshow(frame)
 #print(params)
 plt.plot(X, Y, label='BG')
-plt.plot(X, func(X, a, b, c),
+plt.plot(X, func_poly(X, a, b, c),
          label='Fitted function')
 
 plt.show()
 
-bg_img = get_row(frame1)
+bg_img = get_row(frame)
 
-print(bg_img.shape)
+#print(bg_img.shape)
 
 #io.imread(bg_img)
+
 save_tif(dir_out, bg_img)
+
+#io.imshow(frame, cmap='gray')
+print(bg_img.shape)
 io.imshow(bg_img, cmap='gray')
 
-#fit_data = func(X, a, b, c)
-##fit_data = np.array([list(X), func(X, a, b, c)])
 ##
 #print(len(fit_data))
 #
 #save_csv(dir_out, fit_data)
-##print(list(X), func(X, a, b, c))
 #
-##print(np.array([list(X)], func(X, a, b, c)))
 
 
 
