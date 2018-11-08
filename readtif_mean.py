c6f8052da6cd535aec60653c52a99a4dd1e61ace
SCBera <subhas.9874@gmail.com>
2018-11-08 17:02:06 +0530

    trying def

--- readtif_mean.py
+++ readtif_mean.py
@@ -61,15 +61,14 @@ def make_dir_out(dir_in):
 
 
 def get_filelist(dir_, filetype='*.tif'):
-    """This gets the lists of files in the directory given (excluding the first file)
-    """
+    """This gets the lists of files in the directory given"""
     files = glob.glob(dir_ + filetype)
     # return files[1:] # returns fileslist except the 1st file
     return files
 
 
 def extract_frames_single_file(file_):
-    """This reads .. """
+    """This reads a stack of images from a file and returns dictionary of frames."""
 
     t_dict_single = {}
         
@@ -90,12 +89,12 @@ def extract_frames_single_file(file_):
 
 
 def calculate_mean(t_dict):
-
+    """Reads frames and returns a list of mean intensity values"""
     mean = []
     for t in t_dict:
         slice_ = np.array(t_dict[t])
+        # slice_ = np.array(t_dict[t]).astype(float)
         mean.append(slice_.mean())
-
     return mean
 
 
@@ -103,22 +102,12 @@ def save_csv(dir_out, files, result):
     """This will save resultant values in the output folder as a '.csv' format"""
     try:
         np.savetxt(f"{dir_out}_Individual_mean_of_{len(files)}-files.csv",
-                   result.T, delimiter=",", header=f'{files}')
-                #    result.T, delimiter=",", header=no_of_files)
+                   result, delimiter=",", header=f'{files}')
     except:
         print("Existing csv file is not accessible!")
         exit()
 
-
-if __name__ == "__main__":
-
-    if len(argv) < 2:
-        print("Please specify arguments: '-a' for auto, '-m' for manual.")
-        exit()
-
-    elif argv[1] == '-a':
-        start_1 = time.time()
-
+def mean_auto(info_file = 'info.txt'):
         info_file = open('info.txt')
         for line in info_file:
 
@@ -128,6 +117,7 @@ if __name__ == "__main__":
             dir_ = get_dir(line[:-1])
             # t = int(line[1])
             list_of_files = get_filelist(dir_)
+            
             print(f"\nReading {len(list_of_files)} files from '{dir_[-50:]}'\n")
 
             dir_out = make_dir_out(dir_) # better not to reuse variable names which are same with any function name!
@@ -135,23 +125,31 @@ if __name__ == "__main__":
 
             mean_all = []
             header_all = []
-            for file_ in list_of_files[:]:
+            for file_ in list_of_files:
                 t_dict = extract_frames_single_file(file_)
                 mean = calculate_mean(t_dict)
                 mean_all.append(mean)
-
                 header_all.append(file_[-21:])
-            
-            result = np.array(mean_all)
+            result = np.array(mean_all).T
 
             save_csv(dir_out, header_all, result)
-            
-            # t_dict = 0
-            
-            print("\nTime required(sec): ", time.time() - start_2)
-           
+        print("\nTime required(sec): ", time.time() - start_2)
+         
 
-    elif argv[1] == '-m':
+
+
+if __name__ == "__main__":
+
+    if len(argv) < 2:
+        print("Please specify arguments: '-am' for auto-mean, '-mm' for manual-mean.")
+        exit()
+
+    elif argv[1] == '-am':
+        start_1 = time.time()       
+
+        mean_auto()
+
+    elif argv[1] == '-mm':
         dir_ = get_dir(input("Directory>"))
         # t = int(input("Time point/interval>"))
 
@@ -164,14 +162,12 @@ if __name__ == "__main__":
 
         mean_all = []
         header_all = []
-        for file_ in list_of_files[:]:
+        for file_ in list_of_files:
             t_dict = extract_frames_single_file(file_)
             mean = calculate_mean(t_dict)
             mean_all.append(mean)
-
-            header_all.append(file_[-21:])
-        
-        result = np.array(mean_all)
+            header_all.append(file_[-21:])        
+        result = np.array(mean_all).T
 
         save_csv(dir_out, header_all, result)
 
