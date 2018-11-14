4e4e52e5268e54a1241bd5c0ff5fe4d3866b525c
SCBera <subhas.9874@gmail.com>
2018-11-14 19:03:29 +0530

    showing the plots

--- func.py
+++ func.py
@@ -1,5 +1,5 @@
 """
-This code has the functions: read_info, get_filelist, get_frames, get_men, get_max
+This code has the functions: split_line, get_filelist, get_frames, get_men, get_max
 """
 from skimage import io
 import matplotlib.pyplot as plt
@@ -11,17 +11,20 @@ import psutil
 
 
 def split_line(line):
-    """..."""
+    """Gets directory and time points from a single line of string given"""
     if line.find(',') > -1:
        line = line.split(',')
+       dir_ = line[0]
+       t = line[1]
     else:
-       line = line.split('\n')
-    # try:
-    #     t = int(line[1])
-    # except:
-    #     continue
-    # info_file.close()
-    return line
+        line = line.split('\n')
+        dir_ =line[0]
+        try:
+           t = int(line[1])
+        except:
+            t = None
+            print('Time point not mentioned!')
+    return dir_, t
 
 def get_filelist(dir_, filetype='*.tif'):
     """ This returns lists of files in the directory with given file formate (default is .tif) """
@@ -34,7 +37,7 @@ def get_frames(file_):
     stack = io.imread(file_)
     # print("reading slices...with shape", len(stack.shape))
     if psutil.virtual_memory()[2] > 88: #this to avoid malfuntioning due to memory sortage of the computer
-        print(f'Sytem RAM not sufficient! Higher than 88%!')
+        print(f'Program quits for insufficient RAM! >88%!')
     elif len(stack.shape) < 3: # this analyzes the stack with shape: (y,x)
         t_dict_single = [stack]
     elif stack.shape[0] > 300: # this analyzes the stack with shape: (y,x,z)
@@ -47,7 +50,7 @@ def get_frames(file_):
 
 
 def get_mean(imgfile):
-    """Returns means of mean of a given image file (stack/frame) """
+    """Returns list of mean value(s) of each frame(s) of a given image/stack file"""
     meanlist = []
     if len(imgfile) > 1:
         for slice_pos in imgfile:
@@ -58,7 +61,7 @@ def get_mean(imgfile):
     return meanlist
 
 def get_max(imgfile):
-    """Returns means of mean of a given image file (stack/frame) """
+    """Returns lists of max value(s) of each frame(s) of a given image/stack file"""
     maxlist = []
     if len(imgfile) > 1:
         for slice_pos in imgfile:
@@ -67,6 +70,64 @@ def get_max(imgfile):
     else:
         maxlist = np.array(imgfile).max()
     return maxlist
+
+def make_dir_out(dir_in):
+    """Makes a new directory named '_out' inside the given path."""
+    try:
+        os.makedirs(dir_in + '_out\\', exist_ok=True)
+        dir_out = (dir_in + '_out\\')
+        return dir_out
+    except:
+        raise IOError(f"Unable to make new directory: {dir_in}")
+
+
+def save_csv(dir_out, file_names, result):
+    """This will save the results in the output folder as a '.csv' format. Filename goes to header"""
+    try:
+        np.savetxt(f"{dir_out}_Individual_mean_of_{len(file_names)}-files.csv",
+                   result, delimiter=",", header=f'{file_names}')
+    except:
+        print("Problem with saving csv file!")
+        exit()
+
+
+def show_plot(results):
+
+    # x1 = results[0]
+    y1 = results[0]
+    y2 = results[1]
+    # plt.plot(x1, y1, 'ro')
+    plt.plot(y1, 'ro')
+    plt.plot(y2, "bs")
+    plt.show()
+
+def plot_save_fig(results): #dir_out, no_of_files, results
+    """This will plot figure according to the results and saves the figure in the output folder as a '.png' format"""
+    try:
+        y_value = results[0]
+        fig = plt.figure()
+        plt.plot(y_value, fmt='rs-', linewidth=2, markersize=5, figure = fig)
+        # plt.errorbar(results[0], results[1], yerr = results[2], fmt='rs-', linewidth=2, markersize=5, figure = fig)
+        plt.title('Avrg_int_with_time, SD', fontsize=12)
+        plt.xlabel('Time (h)', fontsize=12)
+        plt.ylabel('Average Int, (Gray value)', fontsize=12)
+        # plt.savefig(f"{dir_out}_avrg_int_with_SD_from_{no_of_files}-files.png")
+        plt.show()
+        plt.close()
+
+        # fig = plt.figure()
+        # plt.errorbar(results[0], results[1], yerr = results[3], fmt='rs-', linewidth=2, markersize=5, figure = fig)
+        # plt.title('Avrg_int_with_time, SEM', fontsize=12)
+        # plt.xlabel('Time (h)', fontsize=12)
+        # plt.ylabel('Average Int, (Gray value)', fontsize=12)
+        # # plt.savefig(f"{dir_out}_avrg_int_with_SE_from_{no_of_files}-files.png")
+        # # plt.show()
+        # # plt.close()
+
+    except:
+        print("Problem with saving figure!")
+        exit()
+
 
 if __name__ == "__main__":
 
--- readtif_test.py
+++ readtif_test.py
@@ -2,27 +2,39 @@ from func import *
 # from func import get_filelist
 # import func
 
-info = open('info.txt')
-# reading file info from file
-for line in info:
-    splited_line = split_line(line)
-    dir_ = splited_line[0]
-    try:
-        t = float(splited_line[1])
-        print(t)
-    except:
-        print('Time point not mentioned!')
-
 # dir_ = input("Dir>")+'\\'
 
-# filelists = get_filelist(dir_)
+info = open('info.txt')
+# reads file info from file
+for line in info:
+    splited_line = split_line(line)
+    dir_ = splited_line[0]+'\\'
+    t = splited_line[1]
 
-# for file_ in filelists:
-#     print(file_)
-#     dict_ = get_frames(file_)
-#     print(len(dict_))
-#     mean = get_mean(dict_)
-#     max = get_max(dict_)
+    # print(dir_, t)
 
-    print(dir_)
+
+    filelists = get_filelist(dir_)
+
+    mn_all = []
+    mx_all = []
+    for file_ in filelists:
+        # print(file_)
+        dict_ = get_frames(file_)
+        # print(len(dict_))
+        mn = get_mean(dict_)
+        mx = get_max(dict_)
+
+        mn_all.append(mn)
+        mx_all.append(mx)
+
+    # print(np.array(mn_all).T, np.array(mx_all).T)
+
+results = [np.array(mn_all), np.array(mx_all)]
+
+
+print(results[1])
+
+show_plot(results[1])
+
 
